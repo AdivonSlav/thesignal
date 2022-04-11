20bae8144612cbb9f4b5124207f812310a0e6de4
Adi <efendic100@gmail.com>
2022-04-11 16:18:53 +0200

    [Feature] Finish research room and implement VFX

---
+++ Assets/Scripts/Scenes/Behaviours/ExtinguishFire.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TheSignal.Scenes.Behaviours
+{
+    public class ExtinguishFire : MonoBehaviour
+    {
+        [SerializeField] private GameObject sprinklerFusebox;
+        [SerializeField] private float healthLoss;
+        [SerializeField] private ParticleSystem smoke;
+        [SerializeField] private ParticleSystem steam;
+
+        private ParticleSystem fire;
+        private SprinklerFix sprinklerFix;
+        private float fireHealth = 100.0f;
+        private bool fireExtinguished = false;
+        
+        private void Awake()
+        {
+            fire = GetComponent<ParticleSystem>();
+            
+            sprinklerFix = sprinklerFusebox.GetComponentInChildren<SprinklerFix>();
+        }
+
+        private void LateUpdate()
+        {
+            if (sprinklerFix.sprinklersStarted)
+            {
+                while (!fireExtinguished)
+                {
+                    smoke.Stop(true);
+                    steam.Play(true);
+                    fireHealth -= healthLoss;
+
+                    if (fireHealth <= 0.0f)
+                    {
+                        fireExtinguished = true;
+                        fire.Stop(true);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes/Behaviours/SprinklerFix.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TheSignal.Player.Input;
+using UnityEngine;
+
+namespace TheSignal.Scenes.Behaviours
+{
+    public class SprinklerFix : MonoBehaviour
+    {
+        [SerializeField] private GameObject player;
+        [SerializeField] private GameObject fixPopup;
+        [SerializeField] private ParticleSystem fuxeboxSparks;
+        [SerializeField] private ParticleSystem[] sprinklerShowers;
+
+        [HideInInspector] public bool sprinklersStarted = false;
+        private InputManager inputManager;
+        private bool playerPresent = false;
+
+        private void Awake()
+        {
+            inputManager = player.GetComponent<InputManager>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (playerPresent)
+            {
+                fixPopup.SetActive(true);
+                
+                if (inputManager.isInteracting)
+                {
+                    fuxeboxSparks.Stop(true);
+
+                    foreach (var sprinkler in sprinklerShowers)
+                    {
+                        sprinkler.Play(true);
+                    }
+
+                    sprinklersStarted = true;
+                    fixPopup.SetActive(false);
+                    this.enabled = false;
+                }
+            }
+            else
+            {
+                fixPopup.SetActive(false);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+                playerPresent = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+                playerPresent = false;
+        }
+    }
+}
